9a174ac2f5a875c8623d223035bf4228aad68ccf
Gaurav Pathak <118663835+gaurav09x@users.noreply.github.com>
2025-05-15 13:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,4 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/feedback', require('./routes/feedback'));
 
+app.get('/', (req, res) => {
+  res.send('Feedback API is running');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
